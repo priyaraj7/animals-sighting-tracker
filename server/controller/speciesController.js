ea3621269e3b8503d4cd4d923898323e0635dc03
Priyaraj7 <suppichala@gmail.com>
2022-04-10 16:31:19 -0700

    done with moving species queries to model

--- server/controller/speciesController.js
+++ server/controller/speciesController.js
@@ -1,6 +1,7 @@
 const db = require("../db/db-connection");
-//  Get all species
+const model = require("../models/species");
 
+//  Get all species
 const getAllSpecies = async (req, res) => {
   try {
     const { rows: species } = await db.query("SELECT * FROM species");
@@ -19,24 +20,16 @@ const addNewSpecies = async (req, res) => {
     conservationStatus: req.body.conservation_status,
     createdOn: req.body.created_on,
   };
-  console.log([newSpecies.commonName, newSpecies.scientificName]);
-  const result = await db.query(
-    "INSERT INTO species(common_name, scientific_name, population, conservation_status, created_on) VALUES($1, $2, $3, $4, $5) RETURNING *",
-    [
-      newSpecies.commonName,
-      newSpecies.scientificName,
-      newSpecies.population,
-      newSpecies.conservationStatus,
-      newSpecies.createdOn,
-    ]
-  );
-  console.log(result.rows[0]);
+  // console.log([newSpecies.commonName, newSpecies.scientificName]);
+
+  const result = await model.addNewSpecies(...newSpecies);
+  // console.log(result.rows[0]);
   res.json(result.rows[0]);
 };
 
 // Put request - Update to an specific species
 const updateSpecies = async (req, res) => {
-  const speciesId = req.params.id;
+  const id = req.params.id;
   const updateSpecies = {
     commonName: req.body.common_name,
     scientificName: req.body.scientific_name,
@@ -44,22 +37,10 @@ const updateSpecies = async (req, res) => {
     conservationStatus: req.body.conservation_status,
     createdOn: req.body.created_on,
   };
-  console.log(
-    "These are the request params that the server is receiving",
-    speciesId
-  );
+  // console.log("These are the request params that the server is receiving", id);
 
-  const query = `UPDATE species SET common_name=$1, scientific_name=$2, population= $3, conservation_status= $4, created_on= $5 WHERE id=${speciesId} RETURNING *`;
-  console.log(query);
-  const values = [
-    updateSpecies.commonName,
-    updateSpecies.scientificName,
-    updateSpecies.population,
-    updateSpecies.conservationStatus,
-    updateSpecies.createdOn,
-  ];
   try {
-    const update = await db.query(query, values);
+    const update = await model.updateSpecies(id, updateSpecies);
     console.log(update.rows[0]);
     res.send(update.rows[0]);
   } catch (e) {
@@ -69,17 +50,14 @@ const updateSpecies = async (req, res) => {
 };
 
 const deleteSpecies = async (req, res) => {
-  const speciesId = req.params.id;
+  const id = req.params.id;
 
-  console.log("Delete request is receiving", speciesId);
+  console.log("Delete request is receiving", id);
 
-  const query = `DELETE FROM species WHERE id = $1`;
-  console.log(query);
-  const values = [speciesId];
   try {
-    const deleteSpecies = await db.query(query, values);
+    const deleteSpecies = await model.deleteSpecies(id);
 
-    res.status(200).send(`species deleted with ID: ${speciesId}`);
+    res.status(200).send(`species deleted with ID: ${id}`);
   } catch (e) {
     console.log(e);
     return res.status(400).json({ e });
--- server/models/species.js
+++ server/models/species.js
@@ -1,21 +1,47 @@
 const db = require("../db/db-connection");
 
-export const getAllSpecies = async () => {
-    const { rows: species } = await db.query("SELECT * FROM species");
-    return species;
-}
+const getAllSpecies = async () => {
+  const { rows: species } = await db.query("SELECT * FROM species");
+  return species;
+};
 
-export const addNewSpecies = async (name, commonName, scientificName, population, conservationStatus ) {
-    const result = await db.query(
-        "INSERT INTO species(common_name, scientific_name, population, conservation_status, created_on) VALUES($1, $2, $3, $4, $5) RETURNING *",
-        [
-          newSpecies.commonName,
-          newSpecies.scientificName,
-          newSpecies.population,
-          newSpecies.conservationStatus,
-          newSpecies.createdOn,
-        ]
-      )
-      return result;
-}
+const addNewSpecies = async (newSpecies) => {
+  const result = await db.query(
+    "INSERT INTO species(common_name, scientific_name, population, conservation_status, created_on) VALUES($1, $2, $3, $4, $5) RETURNING *",
+    [
+      newSpecies.commonName,
+      newSpecies.scientificName,
+      newSpecies.population,
+      newSpecies.conservationStatus,
+      newSpecies.createdOn,
+    ]
+  );
+  return result;
+};
 
+const updateSpecies = async (id, updateSpecies) => {
+  const result = await db.query(
+    `UPDATE species SET common_name=$1, scientific_name=$2, population= $3, conservation_status= $4, created_on= $5 WHERE id= $6 RETURNING *`,
+    [
+      updateSpecies.commonName,
+      updateSpecies.scientificName,
+      updateSpecies.population,
+      updateSpecies.conservationStatus,
+      updateSpecies.createdOn,
+      id,
+    ]
+  );
+  return result;
+};
+
+const deleteSpecies = async (id) => {
+  const result = await db.query(`DELETE FROM species WHERE id = $1`, [id]);
+  return result;
+};
+
+module.exports = {
+  getAllSpecies,
+  addNewSpecies,
+  updateSpecies,
+  deleteSpecies,
+};
